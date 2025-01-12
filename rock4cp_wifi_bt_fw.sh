fc06af73170b7d1634dd31b7b0d066312d655418
herrfrei <herr.frei@gmail.com>
2025-01-12 19:26:06 +0100

    Corrected error

--- rock4cp_wifi_bt_fw.sh
+++ rock4cp_wifi_bt_fw.sh
@@ -15,10 +15,10 @@ pushd /lib/firmware/brcm
 ln -s BCM4345C0_003.001.025.0162.0000_Generic_UART_37_4MHz_wlbga_ref_iLNA_iTR_eLG.hcd BCM4345C0.radxa,rock-4c-plus.hcd
 
 # Todo: add new firmware from Infineon (https://github.com/Infineon/ifx-linux-firmware)
-[ -l brcmfmac43455-sdio.radxa,rock-4c-plus.bin ] && rm -f brcmfmac43455-sdio.radxa,rock-4c-plus.bin
+[ -f brcmfmac43455-sdio.radxa,rock-4c-plus.bin ] && rm -f brcmfmac43455-sdio.radxa,rock-4c-plus.bin
 ln -s ../cypress/cyfmac43455-sdio.bin brcmfmac43455-sdio.radxa,rock-4c-plus.bin
 
-[ -l brcmfmac43455-sdio.radxa,rock-4c-plus.clm_blob ] && rm -f brcmfmac43455-sdio.radxa,rock-4c-plus.clm_blob
+[ -f brcmfmac43455-sdio.radxa,rock-4c-plus.clm_blob ] && rm -f brcmfmac43455-sdio.radxa,rock-4c-plus.clm_blob
 ln -s ../cypress/cyfmac43455-sdio.clm_blob brcmfmac43455-sdio.radxa,rock-4c-plus.clm_blob
 
 popd
